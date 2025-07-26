29c28d0d3e572f869f1f8be9ac4d24c446ff8001
Sikarin <adlivepro@gmail.com>
2025-07-26 15:32:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from routers import device, channels, actions, videos, admin
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "YTFollow API พร้อมใช้งาน"}
+  
 app.include_router(device.router, prefix="/devices")
 app.include_router(channels.router, prefix="/channels")
 app.include_router(actions.router, prefix="/actions")
